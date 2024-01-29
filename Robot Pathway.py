92e3eb494164fc102ffb80ac44f21137ed51b4d5
Chethan Mungara <mnschethan@gmail.com>
2024-01-29 18:03:02 +0530

    Update Robot Pathway.py

--- Robot Pathway.py
+++ Robot Pathway.py
@@ -1,25 +1,11 @@
 import math
-up,down,left,right = 0,0,0,0
+x, y = 0, 0
 while True:
-    lst = input().split()
-    direction = lst[0]
-    if direction == "UP":
-        value = int(lst[1])
-        up += value
-    elif direction == "DOWN":
-        value = int(lst[1])
-        down += value
-    elif direction == "LEFT":
-        value = int(lst[1])
-        left += value
-    elif direction == "RIGHT":
-        value = int(lst[1])
-        right += value
-    elif direction == "STOP":
-        break
-    else:
-        print("invalid Input")
-x = up - down
-y = right - left
-output = math.sqrt((x ** 2) + (y ** 2))
-print(round(output))
+    try:direction, n = input().upper().split()
+    except:break
+    units = int(n)
+    if direction == "UP":y += units
+    elif direction == "DOWN":y -= units
+    elif direction == "LEFT":x -= units
+    elif direction == "RIGHT":x += units
+print(round(math.sqrt((x ** 2) + (y ** 2))))
